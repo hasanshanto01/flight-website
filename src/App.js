ac327fa596351efbebf9d9f6dbcb3fdb0103094d
hasanshanto01 <hasanshanto01@gmail.com>
2023-05-12 01:04:15 +0600

    basic route setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <h2 className="text-3xl font-bold underline text-primary">Hello</h2>
-    </div>
+    <RouterProvider router={router}></RouterProvider>
   );
 }
 
