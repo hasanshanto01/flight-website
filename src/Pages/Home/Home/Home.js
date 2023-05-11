da1242af5f2ce04c6379a4b86209b808352e6d89
hasanshanto01 <hasanshanto01@gmail.com>
2023-05-12 05:56:47 +0600

    mission&vision section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AgencyBanner from '../AgencyBanner/AgencyBanner';
+import MissionAndVision from '../MissionAndVision/MissionAndVision';
 
 const Home = () => {
     return (
         <div className='w-4/5 mx-auto'>
             <AgencyBanner></AgencyBanner>
+            <MissionAndVision></MissionAndVision>
         </div>
     );
 };
---
+++ src/Pages/Home/MissionAndVision/MissionAndVision.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import cardImg from '../../../Assests/card-board.png';
+
+const MissionAndVision = () => {
+    return (
+        <div className='my-16'>
+            <div className='text-center mb-10'>
+                <h2 className='text-2xl font-bold'>Our Mission &  Vision</h2>
+                <p>Serving our customer, searching their entire satisfaction and providing <br />
+                    touristic services of quality, committing to the social, cultural and <br />
+                    environmental reality of our country.</p>
+            </div>
+            <div className='flex justify-evenly'>
+                <div className='w-[350px] h-[350px] relative'>
+                    <p className='text-2xl font-medium absolute top-7 left-16'>Mission</p>
+                    <img src={cardImg} alt="" />
+                    <p className='absolute left-28 bottom-32'>Providing advanced<br />
+                        travel solutions with<br />
+                        great care and<br />
+                        satisfaction.</p>
+                </div>
+                <div className='w-[350px] h-[350px] relative'>
+                    <p className='text-2xl font-medium absolute top-7 left-16'>Vision </p>
+                    <img src={cardImg} alt="" />
+                    <p className='absolute left-28 bottom-32'><span className='text-primary'>Ghurtoni.com</span> is<br />
+                        partnering with you<br />
+                        to reach your dream<br />
+                        destinations.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MissionAndVision;
